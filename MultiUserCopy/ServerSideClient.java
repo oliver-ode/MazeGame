8ddbef8d3ebceff0760df4827b6b61dbdc3386dc
yodasnow <oliverode@gmail.com>
2018-02-13 16:03:27 -0500

    Added server work
    - Added client and server side code

--- MultiUserCopy/ServerSideClient.java
+++ MultiUserCopy/ServerSideClient.java
@@ -1,17 +1,47 @@
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
 
 public class ServerSideClient {
-    public ServerSideClient() throws IOException{
-        /**
-        String str = "Hello\n";
-        BufferedWriter writer = new BufferedWriter(new FileWriter("text.txt", true));
-        writer.append(str);
+    public static final int LISTENING_PORT = 32007;
+    public ServerSideClient() throws IOException {
+        ServerSocket listener;  // Listens for incoming connections.
+        Socket connection;      // For communication with the connecting program.
 
-        writer.close();
-         **/
+
+        try {
+            listener = new ServerSocket(LISTENING_PORT);
+            System.out.println("Listening on port " + LISTENING_PORT);
+            System.out.println(listener.getLocalSocketAddress().toString());
+            System.out.println(listener.getInetAddress().toString());
+            while (true) {
+                // Accept next connection request and handle it.
+                connection = listener.accept();
+                sendDate(connection);
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Sorry, the server has shut down.");
+            System.out.println("Error:  " + e);
+            return;
+        }
+
+
+    }
+    private static void sendDate(Socket client) {
+        try {
+            System.out.println("Connection from " + client.getInetAddress().toString() );
+            PrintWriter outgoing;   // Stream for sending data.
+            outgoing = new PrintWriter( client.getOutputStream() );
+            outgoing.println("Does this thing work?");
+            outgoing.flush();  // Make sure the data is actually sent!
+            client.close();
+        }
+        catch (Exception e){
+            System.out.println("Error: " + e);
+        }
     }
 
     public static void main(String[] args) throws IOException{
