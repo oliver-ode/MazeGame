e4c2a442ee7e11f186e2403f53c2fc5f703f4f19
yodasnow <oliverode@gmail.com>
2018-02-13 16:17:46 -0500

    Added CL-Chat
    Added a code for multi user chat

--- MultiUserCopy/ServerSideClient.java
+++ MultiUserCopy/ServerSideClient.java
@@ -2,7 +2,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Date;
 
 public class ServerSideClient {
     public static final int LISTENING_PORT = 32007;
